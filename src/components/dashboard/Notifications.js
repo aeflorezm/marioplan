41da30a94eb080efd1b1f8d1ed80b35c74033672
aeflorez9707 <andresflorez9707@gmail.com>
2019-12-21 02:43:01 -0500

    dashboard advances

---
+++ src/components/dashboard/Notifications.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const Notifications= ()=>{
+    return(
+        <div>
+            <p>Holi </p>
+        </div>
+    )
+}
+
+export default Notifications
---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+const ProjectList= ()=> {
+    return(
+        <div className= "project-list section">
+            <div className= "card-z-depth-0 project-summary">
+            <div className= "card-content grey-text text-darken-3">
+            <span className= "card-title">Titulo proyecto</span>
+            <p>Posted by: perreo Intenso</p>
+            <p className="grey-text">septiembre 3 del perreo</p>
+        </div>
+        </div>
+
+        <div className= "card-z-depth-0 project-summary">
+            <div className= "card-content grey-text text-darken-3">
+            <span className= "card-title">Titulo proyecto</span>
+            <p>Posted by: perreo Intenso</p>
+            <p className="grey-text">septiembre 3 del perreo</p>
+        </div>
+        </div>
+
+
+        <div className= "card-z-depth-0 project-summary">
+            <div className= "card-content grey-text text-darken-3">
+            <span className= "card-title">Titulo proyecto</span>
+            <p>Posted by: perreo Intenso</p>
+            <p className="grey-text">septiembre 3 del perreo</p>
+        </div>
+        </div>
+
+        </div>
+    )
+}
+export default ProjectList
